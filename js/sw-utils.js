baa0566403ed7346dedf33148ccd716182cffee7
Rodrigo Astrada <rodrigo.astrada@gmail.com>
2020-03-23 09:44:39 -0400

    Modificacion del SW-Utils

--- js/sw-utils.js
+++ js/sw-utils.js
@@ -3,9 +3,11 @@
 // Guardar el caché dinámico
 
 function actualizarCacheDinamico(dynamicCache, req, res) {
-    if (req.ok) {
+    console.log('Req.Ok?', res.ok);
+    if (res.ok) {
         return caches.open(dynamicCache).then(cache => {
             cache.put(req, res.clone());
+            console.log('desde sw-utils - Cache put');
             return res.clone();
         });
     } else {
